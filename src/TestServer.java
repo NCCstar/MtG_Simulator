b063a92911266dbeec7a12b3526bf1845556b40d
Rhys Miller <NCCstar@users.noreply.github.com>
2017-09-27 10:54:21 -0700

    Got client-server connected!

--- src/TestServer.java
+++ src/TestServer.java
@@ -3,24 +3,24 @@ import java.net.*;
 
 public class TestServer {
     public static void main(String[] args) {
-        int portNumber = 1701;
-
-        try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            int i=0;
-            String inputLine;
-            System.out.println(clientSocket.getInetAddress());
-            while ((inputLine = in.readLine()) != null)
-            {
-                System.out.println(i);
-            }
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
+        ServerSocket server;
+        Socket client;
+        DataInputStream input;
+        PrintStream output;
+        try
+        {
+            server = new ServerSocket(9898);
+            System.out.println("Attempting connection");
+            client = server.accept();
+            System.out.println("Connected!");
+            input = new DataInputStream(client.getInputStream());
+            output = new PrintStream(client.getOutputStream());
         }
+        catch(IOException e)
+        {
+            System.out.println(e);
+            client = new Socket();
+        }
+        System.out.println(client.getLocalAddress());
     }
 }
